0dbcaeafa95dfea56c0ed5c39b8fc1797c22e79f
Pavel <p.slavinsy@yandex.ru>
2021-03-19 04:17:09 +0300

    Rebase
    Удалил ненужные ассеты (так и знал что другую ветку нужно было изначально делать)
    Создал игровой уровень,
    Модель крыс и игрока

---
+++ GB_Unity_1.1/Assets/Scripts/player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletStartPosition;
+
+    public GameObject enemy;
+    private Rigidbody _rb;
+
+    private float _speed = 2;
+    private Vector3 _direction;
+
+    //Инициализация объектов
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        var e = enemy.GetComponent<Enemy>();
+        if (e != null)
+        {
+            Debug.Log(e.hp_);
+        }
+    }
+
+    private void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+
+        if (Input.GetMouseButton(0))
+        {
+            Fare();
+        }
+    }
+
+    //Физика
+    private void FixedUpdate()
+    {
+        var speed = _direction * _speed * Time.fixedDeltaTime;
+        transform.Translate(speed);
+    }
+
+    private void LateUpdate()
+    {
+
+    }
+
+    private void Fare()
+    {
+        Instantiate(bullet, bulletStartPosition.position, Quaternion.identity);
+    }
+}
